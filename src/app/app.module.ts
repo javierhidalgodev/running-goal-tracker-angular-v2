0dafa87655c9a03b566d44a29ae11753e423e31a
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-11-12 07:09:43 +0100

    Local cache Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFirestore, initializeFirestore, persistentLocalCache, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
 import { CdkPortalOutlet } from '@angular/cdk/portal';
 import { SharedModule } from '@shared/shared.module';
-import { FooterComponent } from './core/layout/private-layout/components/footer/footer.component';
-import { HeaderComponent } from './core/layout/private-layout/components/header/header.component';
+import { ENABLE_PERSISTENCE } from '@angular/fire/compat/firestore'
 
-const imports = [BrowserModule, AppRoutingModule, SharedModule, CdkPortalOutlet]
+const imports = [
+  BrowserModule,
+  AppRoutingModule,
+  SharedModule,
+  CdkPortalOutlet
+]
 
 @NgModule({
   declarations: [
@@ -19,12 +23,17 @@ const imports = [BrowserModule, AppRoutingModule, SharedModule, CdkPortalOutlet]
   ],
   imports,
   providers: [
-    provideFirebaseApp(() =>
-      initializeApp(environment.firebase)
-    ),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() =>
+      initializeFirestore(
+        getApp(),
+        {
+          localCache: persistentLocalCache() 
+        }
+      )
+    )
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
